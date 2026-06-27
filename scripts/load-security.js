f666fc4e56a549e9807f3e31c423db9e86887041
Kayray651 <ren45689@outlook.com>
2026-06-27 23:46:48 +0800

    Fix: Remove problematic Hexo scripts that broke homepage display

--- scripts/load-security.js
+++
@@ -1,6 +0,0 @@
-hexo.extend.filter.register('after_render:html', function(str, data) {
-    const securityScript = `
-    <script src="/js/security.js"></script>
-    `;
-    return str.replace('</head>', securityScript + '</head>');
-});
